9a2fcd6e2156bc5566e2fa0a99273a3064f3b2c1
mcbeauregard <mcbeauregard@gmail.com>
2018-06-17 10:48:54 +0800

    added concat method

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -11,7 +11,7 @@ class RoomList extends Component {
 
   componentDidMount() {
   this.roomsRef.on('child_added', snapshot => {
-    console.log(snapshot);
+    this.setState({ rooms: this.state.rooms.concat( snapshot.val() ) });
   });
 }
 
