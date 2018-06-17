50b69d1bca8d0722bc84e022d759e2a5e9674e30
mcbeauregard <mcbeauregard@gmail.com>
2018-06-17 09:42:25 +0800

    Stored list of rooms via this.state and added firebase reference

--- components/RoomList.js
+++ components/RoomList.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class Album extends Component {
   constructor(prop) {
     super(props) (
-
+      
+      this.state = {
+        rooms: []
+      };
+      this.roomsRef = this.props.firebase.database().ref('rooms');
     );
 
   }
