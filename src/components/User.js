2d23a8d3e49510254ffcf234b3944990b6d30d0f
mcbeauregard <mcbeauregard@gmail.com>
2018-07-01 10:14:32 +0800

    created signin and signout components and buttons

--- src/components/User.js
+++ src/components/User.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react';
 
 class User extends Component {
 
+signIn = () => {
+  const provider = new this.props.firebase.auth.GoogleAuthProvider();
+  this.props.firebase.auth().signInWithPopup(provider);
+}
+
+signOut = () => {
+  this.props.firebase.auth().signOutWithPopup();
+}
+
+render() {
+  return (
+    <section className="login-menu">
+      <button className="sign-in-button" onClick={this.state.signIn}>'Sign in'</button>
+      <button className="sign-out-button" onClick={this.state.signOut}>'Sign out'</button>
+    </section>
+  );
+}
 }
 
 export default User;
