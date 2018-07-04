1c2b40ce0ceb09c943cf070ec4bb32b55937d7cb
mcbeauregard <mcbeauregard@gmail.com>
2018-07-04 18:34:29 +0800

    new messages adding to active room, user display name not working

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -4,62 +4,61 @@ import './MessageList.css';
 class MessageList extends Component {
   constructor(props){
     super(props);
-    this.state = { messages: []}
+    this.state = { messages: [], message: [], newMessage: ""}
     this.messagesRef = this.props.firebase.database().ref('Messages');
     this.timeStamp = this.props.firebase.database.ServerValue.TIMESTAMP;
     console.log('message displaying');
   }
 
   componentDidMount() {
-    console.log("message component mounted");
-  this.messagesRef.on('child_added', snapshot => {
-    const message = snapshot.val();
-    message.roomId = snapshot.key;
-    this.setState({ messages: this.state.messages.concat(message) })
+    this.messagesRef.on('child_added', snapshot => {
+      const message = snapshot.val();
+      message.key = snapshot.key;
+      this.setState({ messages: this.state.messages.concat(message) })
+    });
+  }
+
+handleSubmitMessage = (e) => { // new method to handle when a new message item is added when user clicks button. Accepts event data.
+  this.messagesRef.push({ // method used in firebase to add an item to a location.
+      content: this.state.newMessage,
+      roomId: this.props.activeRoom.key,
+      username:  this.props.activeUser ? this.props.activeUser.displayName : "Guest",
+      sentAt: this.props.firebase.database.ServerValue.TIMESTAMP
   });
-}
+    this.setState({content: "", roomId:"", sentAt: "", username: ""}); // Updates message list with new message item, if empty leaves field blank.
+  }
 
 handleNewMessage = (e) => {
-  this.setState({ newMessage: e.target.value})
-}
+    this.setState({ newMessage: e.target.value})
+  }
 
-handleSubmitMessage = (newMessage) => { // new method to handle when a new message item is added when user clicks button. Accepts event data.
-  this.messagesRef.push({ // method used in firebase to add an item to a location.
-    message: newMessage
-  });
-  this.setState({ newMessage: ""}); // Updates message list with new message item, if empty leaves field blank.
-}
-
-render() {
-
-  const messageItems = (
-    this.state.messages.map((message, index) => {
-      console.log('Active Room', this.props.activeRoom);
-      console.log('Messages', this.state.Messages);
-    if (this.props.activeRoom && (message.roomId === this.props.activeRoom.key)) {
-      return (
-          <li key={index}>
-            <span className="userinfo">{message.username}</span>
-            <span className="content">{message.content}</span>
-            <span className="time">{message.sentAt}</span>
-          </li>)
-       } else {
-         return false;
-       }
-     })
-     );
-console.log(messageItems);
+  render() {
+    const messageItems = (
+      this.state.messages.map((message, index) => {
+      if (this.props.activeRoom && (message.roomId === this.props.activeRoom.key)) {
+        return (
+            <li key={index}>
+              <span className="userinfo">{message.username}: </span>
+              <span className="content">{message.content} </span>
+              <span className="time">{message.sentAt}</span>
+            </li>)
+         } else {
+           return false;
+         }
+       })
+       );
 
     return (
-      <section className="Message">
-        <div>{messageItems}</div>
-        <form id="createMessageForm" onSubmit={ (e) => { e.preventDefault(); this.handleSubmitMessage(this.state.newMessage) } }>
-          <input type="text" value={ this.state.newMessage } onChange={ this.handleNewMessage} placeholder="Type a new message" />
-          <button>Submit</button>
-        </form>
-      </section>
-    );
+        <section className="Message">
+          <div>{messageItems}</div>
+          <form id="createMessageForm" onSubmit={ (e) => { e.preventDefault(); this.handleSubmitMessage(e)} }>
+            <input type="text" value={ this.state.newMessage} onChange={this.handleNewMessage} placeholder="Type a new message" />
+            <button>Submit</button>
+          </form>
+        </section>
+      );
+    }
   }
-}
+
 
 export default MessageList;
