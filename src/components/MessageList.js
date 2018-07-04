b46a8ab8e4a6f4322f5e39f8853a82bf32427c48
mcbeauregard <mcbeauregard@gmail.com>
2018-07-04 20:40:36 +0800

    attempting to get username to display for new messages

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -4,7 +4,7 @@ import './MessageList.css';
 class MessageList extends Component {
   constructor(props){
     super(props);
-    this.state = { messages: [], message: [], newMessage: ""}
+    this.state = { messages: [], message: [], roomId: "", username: "", newMessage: ""}
     this.messagesRef = this.props.firebase.database().ref('Messages');
     this.timeStamp = this.props.firebase.database.ServerValue.TIMESTAMP;
     console.log('message displaying');
@@ -22,10 +22,10 @@ handleSubmitMessage = (e) => { // new method to handle when a new message item i
   this.messagesRef.push({ // method used in firebase to add an item to a location.
       content: this.state.newMessage,
       roomId: this.props.activeRoom.key,
-      username:  this.props.activeUser ? this.props.activeUser.displayName : "Guest",
+      username: this.state.username,
       sentAt: this.props.firebase.database.ServerValue.TIMESTAMP
-  });
-    this.setState({content: "", roomId:"", sentAt: "", username: ""}); // Updates message list with new message item, if empty leaves field blank.
+      });
+    this.setState({newMessage:""}); // Updates message list with new message item, if empty leaves field blank.
   }
 
 handleNewMessage = (e) => {
@@ -52,8 +52,8 @@ handleNewMessage = (e) => {
         <section className="Message">
           <div>{messageItems}</div>
           <form id="createMessageForm" onSubmit={ (e) => { e.preventDefault(); this.handleSubmitMessage(e)} }>
-            <input type="text" value={ this.state.newMessage} onChange={this.handleNewMessage} placeholder="Type a new message" />
-            <button>Submit</button>
+             <input type="text" value={ this.state.newMessage} onChange={this.handleNewMessage} placeholder="Type a new message" />
+             <button>Submit</button>
           </form>
         </section>
       );
