cf794866311716f92e6047b9972b8f3b0683a303
mcbeauregard <mcbeauregard@gmail.com>
2018-07-06 11:30:38 +0800

    updated date and time format

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -32,6 +32,32 @@ handleNewMessage = (e) => {
     this.setState({ newMessage: e.target.value})
   }
 
+handleDateTime = (timestamp) => {
+  var d =  new Date(timestamp),
+    yyyy = d.getFullYear(),
+    mm = ('0' + (d.getMonth() + 1)).slice(-2),	// Months are zero based. Add leading 0.
+    dd = ('0' + d.getDate()).slice(-2),			// Add leading 0.
+    hh = d.getHours(),
+    h = hh,
+    min = ('0' + d.getMinutes()).slice(-2),		// Add leading 0.
+    ampm = 'AM',
+    time;
+
+    if (hh > 12) {
+    h = hh - 12;
+    ampm = 'PM';
+  } else if (hh === 12) {
+    h = 12;
+    ampm = 'PM';
+  } else if (hh == 0) {
+    h = 12;
+  }
+
+// ie: 2013-02-18, 8:35 AM
+time = yyyy + '-' + mm + '-' + dd + ', ' + h + ':' + min + ' ' + ampm;
+
+return time;
+}
   render() {
     const messageItems = (
       this.state.messages.map((message, index) => {
@@ -40,7 +66,7 @@ handleNewMessage = (e) => {
             <li key={index}>
               <span className="userinfo">{message.username}: </span>
               <span className="content">{message.content} </span>
-              <span className="time">{message.sentAt}</span>
+              <span className="time">{this.handleDateTime(message.sentAt)}</span>
             </li>)
          } else {
            return false;
